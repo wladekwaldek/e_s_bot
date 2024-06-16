ce8fa026aae56baafbf2c9da8b44189a5c40bc68
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-06-16 19:06:10 +0200

    fore

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const TelegramApi = require("node-telegram-bot-api");
 
 const token = process.env.SAVE_MY_ENTRIES;
 
-console.log(token);
-
 const bot = new TelegramApi(token, { polling: true });
 
 bot.on("message", (msg) => {
