dce80e4b36ca5a5016663b254f2a5068199c3cff
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-06-16 18:18:18 +0200

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const key = require("./constants");
+
+const TelegramApi = require("node-telegram-bot-api");
+
+const token = process.env.SAVE_MY_ENTRIES;
+
+const bot = new TelegramApi(token, { polling: true });
+
+bot.on("message", (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "hi");
+});
